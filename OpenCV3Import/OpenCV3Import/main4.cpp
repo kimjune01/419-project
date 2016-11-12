bd918fb6d58d25ed4961497c52dfddcf83df4bf7
Jarrod Haas <JRod@Jarrods-MacBook-Pro.local>
2016-11-11 16:10:25 -0800

    running

--- OpenCV3Import/OpenCV3Import/main4.cpp
+++ OpenCV3Import/OpenCV3Import/main4.cpp
@@ -41,117 +41,22 @@ void capThread(Mat &imgOriginal) {
     
 }
 
-void procThread(Mat &imgOriginal, Mat &imgHSV, Mat &imgThresholded) {
+void capThread2(Mat &imgOriginal) {
     
-    //Convert the captured frame from BGR to HSV
+    //Mat imgOriginal;
     
-    cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV);
+    bool bSuccess = cap.read(imgOriginal);
     
-    inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
-    
-    //morphological opening (removes small objects from the foreground)
-    erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-    dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-    
-    //morphological closing (removes small holes from the foreground)
-    dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-    erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-    
-    //Calculate the moments of the thresholded image
-    Moments oMoments = moments(imgThresholded);
-    
-    double dM01 = oMoments.m01;
-    double dM10 = oMoments.m10;
-    double dArea = oMoments.m00;
-    
-    // if the area <= 10000 assume no object in the image
-    // it's because of the noise, the area is not zero
-    if (dArea > 10000) {
+    if (!bSuccess){
+        cout << "Cannot read a frame from video stream" << endl;
         
-        //calculate the position of the ball
-        int posX = dM10 / dArea;
-        int posY = dM01 / dArea;
-        
-        
-        //Draw a red line from the previous point to the current point
-        if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0) {
-            line(imgLines, Point(posX, posY), Point(iLastX, iLastY), Scalar(0,0,255), 2);
-            
-            
-        }
-        
-        // store current frame data
-        stor_X[frame_count] = posX;
-        stor_Y[frame_count] = posY;
-        stor_VX[frame_count] = posX - iLastX;
-        stor_VY[frame_count] = posY - iLastY;
-        
-        
-        // copy last 100 frames and output to test pipeline
-        if (frame_count > 98 && frame_count < 1000) {
-            
-            // copy last 100 frames to GRT test vector
-            
-            for (int i=0; i < 100; i++) {
-                inputVector[i*4] = stor_X[frame_count-99+i];
-                inputVector[i*4+1] = stor_Y[frame_count-99+i];
-                inputVector[i*4+2] = stor_VX[frame_count-99+i];
-                inputVector[i*4+3] = stor_VY[frame_count-99+i];
-            }
-            
-            
-            cout << "Frame: " << frame_count << endl;
-        }
-        else if (frame_count == 1000) {
-            
-            //copy last 100 frames to first part of page buffer and reset...
-            
-            cout << "Resetting frame buffer: " << endl;
-            
-            for (int i=0; i < 100; i++) {
-                
-                stor_X[i] = stor_X[901+i];
-                stor_Y[i] = stor_Y[901+i];
-                stor_VX[i] = stor_VX[901+i];
-                stor_VY[i] = stor_VY[901+i];
-                
-                inputVector[i*4] = stor_X[i];
-                inputVector[i*4+1] = stor_Y[i];
-                inputVector[i*4+2] = stor_VX[i];
-                inputVector[i*4+3] = stor_VY[i];
-                
-            }
-            
-            targetVector[0] = 0;
-            gestureData.addSample(inputVector, targetVector);
-            
-            
-            // reset the frame counter
-            
-            frame_count = 0;
-            
-        }
-        
-        iLastX = posX;
-        iLastY = posY;
     }
-    
-    // add lines to image
-    imgOriginal = imgOriginal + imgLines;
-    
-    // generate mirror image
-    cv::Mat dst;
-    cv::flip(imgOriginal,dst,0);
-    Point2f src_center(dst.cols/2.0F, dst.rows/2.0F);
-    cv::Mat rot_matrix = getRotationMatrix2D(src_center, 180.0, 1.0);
-    cv::Mat rotated_img(Size(dst.size().height, dst.size().width), dst.type());
-    warpAffine(dst, rotated_img, rot_matrix, dst.size());
     
 }
 
 
 int doMagic() {
-    
+        
     int stor_X[1000];
     int stor_Y[1000];
     int stor_VX[1000];
@@ -187,12 +92,133 @@ int doMagic() {
         // read a new frame from video.
         
         Mat imgOriginal;
-        Mat imgHSV;
-        Mat imgThresholded;
-        
+        Mat imgOriginal2;
         std::thread t1(capThread, std::ref(imgOriginal));
-        std::thread t2(procThread, std::ref(imgOriginal), std::ref(imgHSV), std::ref(imgThresholded));
+        std::thread t2(capThread2, std::ref(imgOriginal2));
         
+        t1.join();
+        t2.join();
+        
+        //Convert the captured frame from BGR to HSV
+        Mat imgHSV;
+        cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV);
+        
+        Mat imgThresholded;
+        inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+        
+        //morphological opening (removes small objects from the foreground)
+        erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+        dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+        
+        //morphological closing (removes small holes from the foreground)
+        dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+        erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+        
+        //Calculate the moments of the thresholded image
+        Moments oMoments = moments(imgThresholded);
+        
+        double dM01 = oMoments.m01;
+        double dM10 = oMoments.m10;
+        double dArea = oMoments.m00;
+        
+        // if the area <= 10000 assume no object in the image
+        // it's because of the noise, the area is not zero
+        if (dArea > 10000) {
+            
+            //calculate the position of the ball
+            int posX = dM10 / dArea;
+            int posY = dM01 / dArea;
+            
+            
+            //Draw a red line from the previous point to the current point
+            if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0) {
+                line(imgLines, Point(posX, posY), Point(iLastX, iLastY), Scalar(0,0,255), 2);
+                
+                
+            }
+            
+            // store current frame data
+            stor_X[frame_count] = posX;
+            stor_Y[frame_count] = posY;
+            stor_VX[frame_count] = posX - iLastX;
+            stor_VY[frame_count] = posY - iLastY;
+            
+            
+            // copy last 100 frames and output to test pipeline
+            if (frame_count > 98 && frame_count < 1000) {
+                
+                // copy last 100 frames to GRT test vector
+                
+                for (int i=0; i < 100; i++) {
+                    inputVector[i*4] = stor_X[frame_count-99+i];
+                    inputVector[i*4+1] = stor_Y[frame_count-99+i];
+                    inputVector[i*4+2] = stor_VX[frame_count-99+i];
+                    inputVector[i*4+3] = stor_VY[frame_count-99+i];
+                }
+                
+                targetVector[0] = 0;
+                gestureData.addSample(inputVector, targetVector);
+                
+                /*cout << "Saving data to file\n";
+                 if( !gestureData.save("RegressionData.csv") ){
+                 cout << "Failed to save training data to CSV file!\n";
+                 return EXIT_FAILURE;
+                 }
+                 break; */
+                
+                
+                cout << "Frame: " << frame_count << endl;
+            }
+            else if (frame_count == 1000) {
+                
+                //copy last 100 frames to first part of page buffer and reset...
+                
+                cout << "Resetting frame buffer: " << endl;
+                
+                for (int i=0; i < 100; i++) {
+                    
+                    stor_X[i] = stor_X[901+i];
+                    stor_Y[i] = stor_Y[901+i];
+                    stor_VX[i] = stor_VX[901+i];
+                    stor_VY[i] = stor_VY[901+i];
+                    
+                    inputVector[i*4] = stor_X[i];
+                    inputVector[i*4+1] = stor_Y[i];
+                    inputVector[i*4+2] = stor_VX[i];
+                    inputVector[i*4+3] = stor_VY[i];
+                    
+                }
+                
+                targetVector[0] = 0;
+                gestureData.addSample(inputVector, targetVector);
+                
+                
+                // reset the frame counter
+                
+                frame_count = 0;
+                
+            }
+            
+            //Output some diagnostic info
+            
+            //cout << "X: " << posX << ", Y: " << posY << endl;
+            //cout << "X-Last: " << abs(posX - iLastX) << ", Y-Last: " << abs(posY-iLastY) << endl;
+            //cout << "capture ticks: " << capture_ticks << endl;
+            
+            iLastX = posX;
+            iLastY = posY;
+        }
+        
+        // add lines to image
+        imgOriginal = imgOriginal + imgLines;
+        
+        // generate mirror image
+        cv::Mat dst;
+        cv::flip(imgOriginal,dst,0);
+        Point2f src_center(dst.cols/2.0F, dst.rows/2.0F);
+        cv::Mat rot_matrix = getRotationMatrix2D(src_center, 180.0, 1.0);
+        cv::Mat rotated_img(Size(dst.size().height, dst.size().width), dst.type());
+        warpAffine(dst, rotated_img, rot_matrix, dst.size());
         
         
         imshow("Thresholded Image", imgThresholded); //show the thresholded image
